9231180a9424ee1e1401771d33b47e76b0811f7f
jananikodithuwakku <163711589+jananikodithuwakku@users.noreply.github.com>
2025-09-01 20:47:43 +0530

    seventh commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import AppRouter from "./AppRouter";
+//import AppRouter from "./AppRouter";
 //import './index.css'
 import App from './App.jsx'
 
