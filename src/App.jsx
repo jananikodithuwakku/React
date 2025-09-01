26394e630f7c3acaef225d60da6df24e8e2122c8
jananikodithuwakku <163711589+jananikodithuwakku@users.noreply.github.com>
2025-09-01 20:43:03 +0530

    Router change

--- src/App.jsx
+++ src/App.jsx
@@ -1,274 +1,83 @@
-import { useEffect, useState, useRef } from 'react'
-import './App.css'
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
+import "./App.css"
 
-//Elements usually returns with one parent element.
+import App1 from "./Pages/App1"
+import App2 from "./Pages/App2"
+import App3 from "./Pages/App3"
+import App4 from "./Pages/App4"
+import App5 from "./Pages/App5"
+import App6 from "./Pages/App6"
+import App7 from "./Pages/App7"
+import App8 from "./Pages/App8"
+import App9 from "./Pages/App9"
+import App10 from "./Pages/App10"
+import App11 from "./Pages/App11"
+import App12 from "./Pages/App12"
+import App13 from "./Pages/App13"
+import App14 from "./Pages/App14"
+import App15 from "./Pages/App15"
+import App16 from "./Pages/App16"
+import App17 from "./Pages/App17"
+import App18 from "./Pages/App18"
+import App19 from "./Pages/App19"
+import App20 from "./Pages/App20"
+import App21 from "./Pages/App21"
+
+
 function App() {
-  
   return (
-    <div>
-      <h1>Hello World</h1>
-      <p>This is paragraph</p>
-    </div>
+    <BrowserRouter>
+        <h2>Navigation Menu</h2>
+        <ul>
+          <li><Link to="/app1">Go to App 1</Link></li>
+          <li><Link to="/app2">Go to App 2</Link></li>
+          <li><Link to="/app3">Go to App 3</Link></li>
+          <li><Link to="/app4">Go to App 4</Link></li>
+          <li><Link to="/app5">Go to App 5</Link></li>
+          <li><Link to="/app6">Go to App 6</Link></li>
+          <li><Link to="/app7">Go to App 7</Link></li>
+          <li><Link to="/app8">Go to App 8</Link></li>
+          <li><Link to="/app9">Go to App 9</Link></li>
+          <li><Link to="/app10">Go to App 10</Link></li>
+          <li><Link to="/app11">Go to App 11</Link></li>
+          <li><Link to="/app12">Go to App 12</Link></li>
+          <li><Link to="/app13">Go to App 13</Link></li>
+          <li><Link to="/app14">Go to App 14</Link></li>
+          <li><Link to="/app15">Go to App 15</Link></li>
+          <li><Link to="/app16">Go to App 16</Link></li>
+          <li><Link to="/app17">Go to App 17</Link></li>
+          <li><Link to="/app18">Go to App 18</Link></li>
+          <li><Link to="/app19">Go to App 19</Link></li>
+          <li><Link to="/app20">Go to App 20</Link></li>
+          <li><Link to="/app21">Go to App 21</Link></li>
+        </ul>
+
+        <Routes>
+          <Route path="/" element={<h1>Welcome to Home</h1>} />
+          <Route path="/app1" element={<App1 />} />
+          <Route path="/app2" element={<App2 />} />
+          <Route path="/app3" element={<App3 />} />
+          <Route path="/app4" element={<App4 />} />
+          <Route path="/app5" element={<App5 />} />
+          <Route path="/app6" element={<App6 />} />
+          <Route path="/app7" element={<App7 />} />
+          <Route path="/app8" element={<App8 />} />
+          <Route path="/app9" element={<App9 />} />
+          <Route path="/app10" element={<App10 />} />
+          <Route path="/app11" element={<App11 />} />
+          <Route path="/app12" element={<App12 />} />
+          <Route path="/app13" element={<App13 />} />
+          <Route path="/app14" element={<App14 />} />
+          <Route path="/app15" element={<App15 />} />
+          <Route path="/app16" element={<App16 />} />
+          <Route path="/app17" element={<App17 />} />
+          <Route path="/app18" element={<App18 />} />
+          <Route path="/app19" element={<App19 />} />
+          <Route path="/app20" element={<App20 />} />
+          <Route path="/app21" element={<App21 />} />
+        </Routes>
+    </BrowserRouter>
   )
 }
 
-//Elements can be returned same as variables or constants.
-function App2(){
-  const element = <h2>Hello</h2>
-  return element
-}
-
-function App3(){
-  const e_1 = <h2>Hello</h2>
-  const e_2 = <h2>World</h2>
-  return [e_1 ,e_2]
-}
-//Multiple elements with no parent should be an array or use an empty element.
-function App4(){
-  return(
-    <>
-      <h2>Hello</h2>
-      <h2>World</h2>
-    </>
-  )
-}
-
-function App5(){
-  const array = ["cat", "Dog", "Elephant", "Deer"]
-  const elements = []
-  for (let i= 0; i < array.length; i++){
-    elements.push(
-      <button key = {i}>  {array[i]} </button>
-    )
-  }
-  return (
-    <div> <h3> Animal </h3> {elements} </div>
-  )
-}
-
-//Much easier to generate list using mapping
-function App6(){
-  const array = ["cat", "Dog", "Elephant", "Deer"]
-  return(
-    <div><h3>Animal</h3>{
-        array.map ((item, index) =>(
-          <button key = {index}> {item} </button>
-      ))
-    }</div>
-
-  )
-}
-
-//using a function inside a component to handle user actions.
-function App7(){
-  function myFunction(){
-    alert("You Click me")
-  }
-  return(
-    <div><button onClick={myFunction}>Click this</button></div>
-  )
-}
-
-function MyComponent(){
-  return(
-    <div>Simple Text</div>
-  )
-}
-function App8(){
-  return(
-    <div><h2>Reusing Components</h2>
-      <MyComponent/>
-      <MyComponent/>
-    </div>
-  )
-}
-
-function MyComponent1(props){
-  return(
-    <div><h1>Name: {props.Name}</h1>
-    <p><h3>Age: {props.Age}</h3></p>
-    </div>
-  )
-}
-function App9(){
-  return(
-    <div>
-      <MyComponent1 Name= "Jhon" Age = {32}/>
-      <MyComponent1 Name= "Jack" Age = {24}/>
-    </div>
-  )
-}
-
-
-function MyComponent2(props) {
-  return (
-    <div>
-      <h1>
-        Name: {props.name}
-      </h1>
-      <button onClick={() => props.selectItem(props.id)}>
-        Select
-      </button>
-    </div>
-  )
-}
-function App10() {
-  function select(id) {
-    alert("You selected", id)
-  }
-  return (
-    <div>
-      <MyComponent2 id={10} name="John" selectItem={select} />
-      <MyComponent2 id={15} name="Jack" selectItem={select} />
-    </div>
-  )
-}
-
-//Conditional Rendering
-function App11(){
-  const element_1 = <h2>Hello</h2>
-  const element_2 = <h2>World</h2>
-
-  if (1 === 2) {
-    return element_1
-  }
-  else{
-    return element_2
-  }
-}
-
-function App12(){
-  return 1 === 2
-  ? <h3>Hello</h3>
-  : <h3>World</h3>
-
-}
-
-function App13(){
-  const child = 1 === 2
-  ? <h3>World</h3>
-  : null
-
-  return (
-    <div><h3>Hello</h3>{child}</div>
-  )
-
-}
-
-function App14(){
-  return (
-    <div><h1>Hello</h1>
-      {
-        1 === 2
-          ? <h3>World</h3>
-          : null
-      }
-    </div>
-  )
-}
-
-function App15(){
-  return(
-    <div><h1>World</h1>
-      {(1 === 2) && <h2>Hello</h2> }
-    </div>
-  )
-}
-
-//React Hooks
-//useState
-function App16(){
-  const [counter, setCounter] = useState(0)
-
-  return (
-    <div><h2>You clicked {counter} times</h2>
-    <button onClick={() => setCounter(counter + 1)}>Click me</button>
-    </div>
-  )
-}
-
-function App17(){
-  const [counter, setCounter] = useState(0)
-  const [time, setTime] = useState(0)
-  function clickEvent(){
-    setCounter(counter + 1)
-    setTime(Date.now())
-  }
-
-  return(
-    <div>
-      <h2>You clicked {counter} times</h2>
-      <h3>Last Clicked time: {time}</h3>
-      <button onClick={clickEvent}>Click me</button>
-    </div>
-  )
-}
-
-function App18(){
-  const [name, setName] =useState("")
-  const [age, setAge] =useState(18)
-  const [gender, setGender] =useState("male")
-  const [registerde, setRegistered] =useState("false")
-
-  return(
-    <form>
-      <input type="text" value={name} onChange={event => setName(event.target.value)} />
-
-      <input type="number" value={age} onChange={event => setAge(event.target.value)} />
-
-      <select value={gender} onChange={event => setGender(event.target.value)}>
-        <option value="male">Male</option>
-        <option value="female">Female</option>
-      </select>
-
-      <label>
-        <input type="checkbox" value={registerde} onChange={event => setRegistered(event.target.value)} />
-      </label>
-
-    </form>
-  )
-  
-}
-
-//useEffect
-function App19(){
-  const [counter, setCounter] = useState(0)
-
-  useEffect(()=> { console.log("You click the button")}, [counter])
-
-  return (
-    <div><h2>You clicked {counter} times</h2>
-    <button onClick={() => setCounter(counter + 1)}>Click me</button>
-    </div>
-  )
-}
-
-function App20() {
-  const [counter, setCounter] = useState(0)
-
-  useEffect(() => { console.log("Component mounted!")}, [])
-
-  return (
-    <div>
-      <h1>You clicked {counter} times!</h1>
-      <button onClick={() => setCounter(counter + 1)}>Click Me</button>
-    </div>
-  )
-}
-
-function App21() {
-  const counter = useRef(0)
-  
-  function clickEvent() {
-    counter.current += 1 
-    console.log("You clicked ", counter.current, "times")
-  }
-
-  return (
-    <div>
-      <button onClick={clickEvent}>Click Me</button>
-    </div>
-  )
-}
-
-export default App9
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppRouter from "./AppRouter";
 //import './index.css'
-//import App from './App.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppRouter />
+    <App />
   </StrictMode>,
 )
